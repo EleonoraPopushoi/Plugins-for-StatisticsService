773734a6d8baa9aa10b719c1510c74fb83918365
Eleonora Popushoi <eleonorapopushoi@gmail.com>
2020-09-11 15:44:41 +0300

    Updated files

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -7,6 +7,15 @@ public class StatisticsService {
      * @param incomes - array of incomes
      * @return - index of first max value
      */
+
+    public long calculateSum(long[] incomes) {
+        long sum = 0;
+        for (long purchase : incomes) {
+            sum = sum + purchase;
+        }
+        return sum;
+    }
+
     public long findMax(long[] incomes) {
         long current_max_index = 0;
         long current_max = incomes[0];
@@ -14,5 +23,52 @@ public class StatisticsService {
             if (current_max < income)
                 current_max = income;
         return current_max;
+    }
+
+
+    public long findMin(long[] incomes) {
+        long current_min_index = 0;
+        long current_min = incomes[0];
+        for (long income : incomes)
+            if (current_min > income)
+                current_min = income;
+        return current_min;
+    }
+
+    public long findAverage(long[] purchases) {
+        long sum = 0;
+        for (long d : purchases) {
+            // d is defined as length
+            sum += d;
+        }
+
+        return sum / purchases.length;
+
+    }
+
+    public long MonthsBelowAveragePurchase(long[] incomes) {
+
+        long average = findAverage(incomes);
+        long NumberMonthsBelowAverage = 0;
+        for (long belowaveragepurchase : incomes) {
+            if (belowaveragepurchase < average) {
+                NumberMonthsBelowAverage++;
+            }
+        }
+        return NumberMonthsBelowAverage;
+
+    }
+
+
+    public long MonthsAboveAveragePurchase(long[] incomes) {
+        long average = findAverage(incomes);
+        long NumberMonthsAboveAverage = 0;
+
+        for (long aboveaveragepurchase : incomes) {
+            if (aboveaveragepurchase > average) {
+                NumberMonthsAboveAverage++;
+            }
+        }
+        return NumberMonthsAboveAverage;
     }
 }
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,15 +6,70 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
 
+    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+
+    @Test
+    void calculateSum() {
+        StatisticsService service = new StatisticsService();
+
+        long expected = 85;
+        long actual = service.calculateSum(incomesInBillions);
+
+        assertEquals(expected , actual);
+    }
+
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
     }
+
+
+    @Test
+    void findMin() {
+        StatisticsService service = new StatisticsService();
+
+        long expected = 3;
+        long actual = service.findMin(incomesInBillions);
+
+        assertEquals(expected , actual);
+    }
+
+    @Test
+    void findAverage() {
+        StatisticsService service = new StatisticsService();
+        long expected = 7;
+        long actual = service.findAverage(incomesInBillions);
+
+        assertEquals(expected , actual);
+    }
+
+    @Test
+    void MonthsBelowAveragePurchase() {
+
+        StatisticsService service = new StatisticsService();
+
+        long expected = 5;
+        long actual = service.MonthsBelowAveragePurchase(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void MonthsAboveAverageOfSum() {
+        StatisticsService service = new StatisticsService();
+
+        long expected = 6;
+        long actual = service.MonthsAboveAveragePurchase(incomesInBillions);
+
+        assertEquals(expected, actual);
+
+    }
+
+
 }
